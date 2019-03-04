7ef4eed2635016b1210e16f08b0d6fd8c3b4ee2a
TomDev24 <mail.com67747@gmail.com>
2019-03-04 10:26:11 +0300

    Updates

---
+++ precache-manifest.da9a4ec7780305f11caf17d0edac1a66.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ravenous/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "0b9199a502634757a9bf",
+    "url": "/ravenous/static/js/runtime~main.0b9199a5.js"
+  },
+  {
+    "revision": "96b3afc0be43562978c7",
+    "url": "/ravenous/static/js/main.96b3afc0.chunk.js"
+  },
+  {
+    "revision": "b8324b05b1214c6118dc",
+    "url": "/ravenous/static/js/2.b8324b05.chunk.js"
+  },
+  {
+    "revision": "96b3afc0be43562978c7",
+    "url": "/ravenous/static/css/main.548e9aaa.chunk.css"
+  },
+  {
+    "revision": "1c051f35f4aca3879b8a844c84ecd787",
+    "url": "/ravenous/index.html"
+  }
+];
